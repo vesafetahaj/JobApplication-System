417d116294de59cb29d9fb29fc7ff227c37de064
Vesa <vf58160@ubt-uni.net>
2023-12-24 22:07:09 +0100

    Login&Register front and back done

---
+++ JobApplicationSystem/wwwroot/js/login.js
@@ -0,0 +1,8 @@
+function openModal() {
+    document.getElementById("login-modal").style.display = "flex";
+    document.getElementById("login-box").style.animation = "fadeIn 0.5s ease-out";
+}
+
+function closeModal() {
+    document.getElementById("login-modal").style.display = "none";
+}
